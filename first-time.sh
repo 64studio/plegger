9ab2533173050605fd4d9e28d55704fc272f903d
Chris Obbard <obbardc@gmail.com>
2017-04-21 17:17:53 +0100

    bug fixes

--- first-time.sh
+++ first-time.sh
@@ -8,7 +8,7 @@ sudo systemctl recorder stop
 cd /usr/share/plegger/webapp
 sudo npm -g install browserify
 sudo npm install
-sudo browserify src/client/plegger_client.js -t babelify --outfile web/pleg_cfe.js -D
+sudo browserify src/client/plegger_client.js -t babelify --outfile web/pleg_cfe.js -d
 
 sudo systemctl webapp start
 sudo systemctl recorder start
